c1c1a8d9b450cda9484ffa033b8b942737ce8d9f
Ebrahim Ramadan <65041082+Ebrahim-Ramadan@users.noreply.github.com>
2025-05-29 07:39:31 +0200

    Update jsontocockroachdb.py

--- jsontocockroachdb.py
+++ jsontocockroachdb.py
@@ -4,7 +4,7 @@ from urllib.parse import quote_plus
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://sharmojj:Sharmover%40100@cluster0.1snfcdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://sharmojj:ass@cluster0.1snfcdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
